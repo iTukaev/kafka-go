bd2ee38fe6ebd1db0137139ec924fe7e4456b591
Ильдус Тукаев <i.tukaev@tbank.ru>
2024-12-09 16:18:00 +0500

    init

---
+++ gzip/gzip.go
@@ -0,0 +1,24 @@
+// Package gzip does nothing, it's kept for backward compatibility to avoid
+// breaking the majority of programs that imported it to install the compression
+// codec, which is now always included.
+package gzip
+
+import (
+	"github.com/iTukaev/kafka-go/compress/gzip"
+	gz "github.com/klauspost/compress/gzip"
+)
+
+const (
+	Code                    = 1
+	DefaultCompressionLevel = gz.DefaultCompression
+)
+
+type CompressionCodec = gzip.Codec
+
+func NewCompressionCodec() *CompressionCodec {
+	return NewCompressionCodecLevel(DefaultCompressionLevel)
+}
+
+func NewCompressionCodecLevel(level int) *CompressionCodec {
+	return &CompressionCodec{Level: level}
+}
---
+++ protocol/describeuserscramcredentials/describeuserscramcredentials_test.go
@@ -0,0 +1,41 @@
+package describeuserscramcredentials_test
+
+import (
+	"testing"
+
+	"github.com/iTukaev/kafka-go/protocol/describeuserscramcredentials"
+	"github.com/iTukaev/kafka-go/protocol/prototest"
+)
+
+const (
+	v0 = 0
+)
+
+func TestDescribeUserScramCredentialsRequest(t *testing.T) {
+	prototest.TestRequest(t, v0, &describeuserscramcredentials.Request{
+		Users: []describeuserscramcredentials.RequestUser{
+			{
+				Name: "foo-1",
+			},
+		},
+	})
+}
+
+func TestDescribeUserScramCredentialsResponse(t *testing.T) {
+	prototest.TestResponse(t, v0, &describeuserscramcredentials.Response{
+		ThrottleTimeMs: 500,
+		Results: []describeuserscramcredentials.ResponseResult{
+			{
+				User:         "foo",
+				ErrorCode:    1,
+				ErrorMessage: "foo-error",
+				CredentialInfos: []describeuserscramcredentials.CredentialInfo{
+					{
+						Mechanism:  2,
+						Iterations: 15000,
+					},
+				},
+			},
+		},
+	})
+}
